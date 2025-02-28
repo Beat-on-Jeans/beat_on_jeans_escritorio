21dc14a12c22957523e76dd9282f41ef6bceb677
tonotpanda <161296297+tonotpanda@users.noreply.github.com>
2025-02-28 10:39:55 +0100

    crear homes de todos los usuarios y que los datagridviews salgan con border redondos.

---
+++ beat_on_jeans_escritorio/FormHomeMantenimiento.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace beat_on_jeans_escritorio
+{
+    partial class FormHomeMantenimiento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.dataGridViewGestionMusicosLocales = new System.Windows.Forms.DataGridView();
+            this.dataGridViewListaModificaciones = new System.Windows.Forms.DataGridView();
+            this.dataGridViewHistorialModificaciones = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGestionMusicosLocales)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewListaModificaciones)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewHistorialModificaciones)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Figtree Medium", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(512, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gestión músicos y locales";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Figtree Medium", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(557, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(413, 48);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lista modificaciones";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Figtree Medium", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 429);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(482, 48);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Historial modificaciones";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::beat_on_jeans_escritorio.Properties.Resources.rectanguloNaranjita;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 84);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(503, 326);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::beat_on_jeans_escritorio.Properties.Resources.rectanguloNaranjita;
+            this.pictureBox2.Location = new System.Drawing.Point(20, 495);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(503, 463);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::beat_on_jeans_escritorio.Properties.Resources.rectanguloNaranjita;
+            this.pictureBox3.Location = new System.Drawing.Point(565, 84);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(517, 874);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // dataGridViewGestionMusicosLocales
+            // 
+            this.dataGridViewGestionMusicosLocales.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(226)))));
+            this.dataGridViewGestionMusicosLocales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewGestionMusicosLocales.Location = new System.Drawing.Point(20, 84);
+            this.dataGridViewGestionMusicosLocales.Name = "dataGridViewGestionMusicosLocales";
+            this.dataGridViewGestionMusicosLocales.RowHeadersWidth = 51;
+            this.dataGridViewGestionMusicosLocales.RowTemplate.Height = 24;
+            this.dataGridViewGestionMusicosLocales.Size = new System.Drawing.Size(503, 326);
+            this.dataGridViewGestionMusicosLocales.TabIndex = 6;
+            // 
+            // dataGridViewListaModificaciones
+            // 
+            this.dataGridViewListaModificaciones.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(226)))));
+            this.dataGridViewListaModificaciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewListaModificaciones.Location = new System.Drawing.Point(565, 84);
+            this.dataGridViewListaModificaciones.Name = "dataGridViewListaModificaciones";
+            this.dataGridViewListaModificaciones.RowHeadersWidth = 51;
+            this.dataGridViewListaModificaciones.RowTemplate.Height = 24;
+            this.dataGridViewListaModificaciones.Size = new System.Drawing.Size(517, 874);
+            this.dataGridViewListaModificaciones.TabIndex = 7;
+            // 
+            // dataGridViewHistorialModificaciones
+            // 
+            this.dataGridViewHistorialModificaciones.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(226)))));
+            this.dataGridViewHistorialModificaciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewHistorialModificaciones.Location = new System.Drawing.Point(20, 495);
+            this.dataGridViewHistorialModificaciones.Name = "dataGridViewHistorialModificaciones";
+            this.dataGridViewHistorialModificaciones.RowHeadersWidth = 51;
+            this.dataGridViewHistorialModificaciones.RowTemplate.Height = 24;
+            this.dataGridViewHistorialModificaciones.Size = new System.Drawing.Size(503, 463);
+            this.dataGridViewHistorialModificaciones.TabIndex = 8;
+            // 
+            // FormHomeMantenimiento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1121, 990);
+            this.Controls.Add(this.dataGridViewHistorialModificaciones);
+            this.Controls.Add(this.dataGridViewListaModificaciones);
+            this.Controls.Add(this.dataGridViewGestionMusicosLocales);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormHomeMantenimiento";
+            this.Text = "FormHomeMantenimiento";
+            this.Load += new System.EventHandler(this.FormHomeMantenimiento_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGestionMusicosLocales)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewListaModificaciones)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewHistorialModificaciones)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.DataGridView dataGridViewGestionMusicosLocales;
+        private System.Windows.Forms.DataGridView dataGridViewListaModificaciones;
+        private System.Windows.Forms.DataGridView dataGridViewHistorialModificaciones;
+    }
+}
---
+++ beat_on_jeans_escritorio/FormHomeSuperUsuario.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace beat_on_jeans_escritorio
+{
+    partial class FormHomeSuperUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.dataGridViewNumUsuarios = new System.Windows.Forms.DataGridView();
+            this.dataGridViewUltimosRegistros = new System.Windows.Forms.DataGridView();
+            this.dataGridViewGestionUsuarios = new System.Windows.Forms.DataGridView();
+            this.dataGridViewNotificaciones = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewNumUsuarios)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUltimosRegistros)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGestionUsuarios)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewNotificaciones)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Figtree Medium", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(446, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Estadisticas generales";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Figtree Medium", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(564, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(345, 48);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Últimos registros";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(226)))));
+            this.label3.Font = new System.Drawing.Font("Figtree Medium", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(380, 41);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Número total usuarios";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Figtree Medium", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 422);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(337, 48);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Gestión usuarios";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Figtree Medium", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(564, 559);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(444, 48);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Últimas notificaciones";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::beat_on_jeans_escritorio.Properties.Resources.rectanguloNaranjita;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 80);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(503, 326);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::beat_on_jeans_escritorio.Properties.Resources.rectanguloNaranjita;
+            this.pictureBox2.Location = new System.Drawing.Point(572, 80);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(517, 463);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::beat_on_jeans_escritorio.Properties.Resources.rectanguloNaranjita;
+            this.pictureBox3.Location = new System.Drawing.Point(20, 484);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(503, 463);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::beat_on_jeans_escritorio.Properties.Resources.rectanguloNaranjita;
+            this.pictureBox4.Location = new System.Drawing.Point(572, 621);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(517, 326);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 8;
+            this.pictureBox4.TabStop = false;
+            // 
+            // dataGridViewNumUsuarios
+            // 
+            this.dataGridViewNumUsuarios.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(226)))));
+            this.dataGridViewNumUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewNumUsuarios.Location = new System.Drawing.Point(20, 134);
+            this.dataGridViewNumUsuarios.Name = "dataGridViewNumUsuarios";
+            this.dataGridViewNumUsuarios.RowHeadersWidth = 51;
+            this.dataGridViewNumUsuarios.RowTemplate.Height = 24;
+            this.dataGridViewNumUsuarios.Size = new System.Drawing.Size(503, 272);
+            this.dataGridViewNumUsuarios.TabIndex = 9;
+            // 
+            // dataGridViewUltimosRegistros
+            // 
+            this.dataGridViewUltimosRegistros.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(226)))));
+            this.dataGridViewUltimosRegistros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewUltimosRegistros.Location = new System.Drawing.Point(572, 80);
+            this.dataGridViewUltimosRegistros.Name = "dataGridViewUltimosRegistros";
+            this.dataGridViewUltimosRegistros.RowHeadersWidth = 51;
+            this.dataGridViewUltimosRegistros.RowTemplate.Height = 24;
+            this.dataGridViewUltimosRegistros.Size = new System.Drawing.Size(517, 463);
+            this.dataGridViewUltimosRegistros.TabIndex = 10;
+            // 
+            // dataGridViewGestionUsuarios
+            // 
+            this.dataGridViewGestionUsuarios.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(226)))));
+            this.dataGridViewGestionUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewGestionUsuarios.Location = new System.Drawing.Point(20, 484);
+            this.dataGridViewGestionUsuarios.Name = "dataGridViewGestionUsuarios";
+            this.dataGridViewGestionUsuarios.RowHeadersWidth = 51;
+            this.dataGridViewGestionUsuarios.RowTemplate.Height = 24;
+            this.dataGridViewGestionUsuarios.Size = new System.Drawing.Size(503, 463);
+            this.dataGridViewGestionUsuarios.TabIndex = 11;
+            // 
+            // dataGridViewNotificaciones
+            // 
+            this.dataGridViewNotificaciones.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(226)))));
+            this.dataGridViewNotificaciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewNotificaciones.Location = new System.Drawing.Point(572, 621);
+            this.dataGridViewNotificaciones.Name = "dataGridViewNotificaciones";
+            this.dataGridViewNotificaciones.RowHeadersWidth = 51;
+            this.dataGridViewNotificaciones.RowTemplate.Height = 24;
+            this.dataGridViewNotificaciones.Size = new System.Drawing.Size(517, 326);
+            this.dataGridViewNotificaciones.TabIndex = 12;
+            // 
+            // FormHomeSuperUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1116, 972);
+            this.Controls.Add(this.dataGridViewNotificaciones);
+            this.Controls.Add(this.dataGridViewGestionUsuarios);
+            this.Controls.Add(this.dataGridViewUltimosRegistros);
+            this.Controls.Add(this.dataGridViewNumUsuarios);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FormHomeSuperUsuario";
+            this.Text = "FormHomeSuperUsuario";
+            this.Load += new System.EventHandler(this.FormHomeSuperUsuario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewNumUsuarios)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUltimosRegistros)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewGestionUsuarios)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewNotificaciones)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.DataGridView dataGridViewNumUsuarios;
+        private System.Windows.Forms.DataGridView dataGridViewUltimosRegistros;
+        private System.Windows.Forms.DataGridView dataGridViewGestionUsuarios;
+        private System.Windows.Forms.DataGridView dataGridViewNotificaciones;
+    }
+}
