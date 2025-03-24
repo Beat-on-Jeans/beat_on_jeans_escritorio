4d3c760b5a77922a6baf841149b858a658aaa49f
facu <facundocapurata@gmail.com>
2025-03-24 17:59:39 +0100

    Archivo Orm y T4

---
+++ beat_on_jeans_escritorio/Models/Orm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beat_on_jeans_escritorio.Models
+{
+    public static class Orm
+    {
+        public static dam05Entities db = new dam05Entities();
+    }
+}
