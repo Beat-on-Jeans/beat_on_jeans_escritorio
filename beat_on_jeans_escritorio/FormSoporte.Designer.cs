9f63a9eb84b9ec198fb056b7d5d127456386f250
tonotpanda <jcastillop2324@politecnics.barcelona>
2025-03-27 18:50:40 +0100

    agregar formulario soporte mas diseño.

--- beat_on_jeans_escritorio/FormSoporte.Designer.cs
+++ beat_on_jeans_escritorio/FormSoporte.Designer.cs
@@ -28,12 +28,219 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridViewTickets = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Usuario_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tecnico_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TipoIncidencia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fecha_Creacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fecha_Cierre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.labelNombreTecnico = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelNombreUsuario = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonCerrarTicket = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTickets)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewTickets
+            // 
+            this.dataGridViewTickets.AllowUserToAddRows = false;
+            this.dataGridViewTickets.AllowUserToDeleteRows = false;
+            this.dataGridViewTickets.AllowUserToOrderColumns = true;
+            this.dataGridViewTickets.AllowUserToResizeColumns = false;
+            this.dataGridViewTickets.AllowUserToResizeRows = false;
+            this.dataGridViewTickets.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(226)))));
+            this.dataGridViewTickets.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridViewTickets.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(104)))), ((int)(((byte)(27)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Poppins", 8F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(104)))), ((int)(((byte)(27)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewTickets.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridViewTickets.ColumnHeadersHeight = 40;
+            this.dataGridViewTickets.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Usuario_ID,
+            this.Tecnico_ID,
+            this.TipoIncidencia,
+            this.Fecha_Creacion,
+            this.Fecha_Cierre});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(191)))), ((int)(((byte)(105)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Poppins", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(220)))), ((int)(((byte)(150)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewTickets.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridViewTickets.EnableHeadersVisualStyles = false;
+            this.dataGridViewTickets.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(243)))), ((int)(((byte)(226)))));
+            this.dataGridViewTickets.Location = new System.Drawing.Point(22, 28);
+            this.dataGridViewTickets.Name = "dataGridViewTickets";
+            this.dataGridViewTickets.ReadOnly = true;
+            this.dataGridViewTickets.RowHeadersVisible = false;
+            this.dataGridViewTickets.RowHeadersWidth = 25;
+            this.dataGridViewTickets.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewTickets.Size = new System.Drawing.Size(661, 469);
+            this.dataGridViewTickets.TabIndex = 0;
+            // 
+            // ID
+            // 
+            this.ID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ID.FillWeight = 90F;
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // Usuario_ID
+            // 
+            this.Usuario_ID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Usuario_ID.FillWeight = 90F;
+            this.Usuario_ID.HeaderText = "ID Usuario";
+            this.Usuario_ID.Name = "Usuario_ID";
+            this.Usuario_ID.ReadOnly = true;
+            // 
+            // Tecnico_ID
+            // 
+            this.Tecnico_ID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Tecnico_ID.FillWeight = 90F;
+            this.Tecnico_ID.HeaderText = "ID Técnico";
+            this.Tecnico_ID.Name = "Tecnico_ID";
+            this.Tecnico_ID.ReadOnly = true;
+            // 
+            // TipoIncidencia
+            // 
+            this.TipoIncidencia.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.TipoIncidencia.FillWeight = 150F;
+            this.TipoIncidencia.HeaderText = "Tipo de incidencia";
+            this.TipoIncidencia.Name = "TipoIncidencia";
+            this.TipoIncidencia.ReadOnly = true;
+            // 
+            // Fecha_Creacion
+            // 
+            this.Fecha_Creacion.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Fecha_Creacion.FillWeight = 150F;
+            this.Fecha_Creacion.HeaderText = "Fecha Creación";
+            this.Fecha_Creacion.Name = "Fecha_Creacion";
+            this.Fecha_Creacion.ReadOnly = true;
+            // 
+            // Fecha_Cierre
+            // 
+            this.Fecha_Cierre.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Fecha_Cierre.FillWeight = 150F;
+            this.Fecha_Cierre.HeaderText = "Fecha Cierre";
+            this.Fecha_Cierre.Name = "Fecha_Cierre";
+            this.Fecha_Cierre.ReadOnly = true;
+            // 
+            // labelNombreTecnico
+            // 
+            this.labelNombreTecnico.AutoSize = true;
+            this.labelNombreTecnico.Font = new System.Drawing.Font("Figtree Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNombreTecnico.Location = new System.Drawing.Point(689, 65);
+            this.labelNombreTecnico.Name = "labelNombreTecnico";
+            this.labelNombreTecnico.Size = new System.Drawing.Size(50, 20);
+            this.labelNombreTecnico.TabIndex = 1;
+            this.labelNombreTecnico.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(689, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nombre del técnico";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(689, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre del usuario";
+            // 
+            // labelNombreUsuario
+            // 
+            this.labelNombreUsuario.AutoSize = true;
+            this.labelNombreUsuario.Font = new System.Drawing.Font("Figtree Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNombreUsuario.Location = new System.Drawing.Point(690, 153);
+            this.labelNombreUsuario.Name = "labelNombreUsuario";
+            this.labelNombreUsuario.Size = new System.Drawing.Size(53, 20);
+            this.labelNombreUsuario.TabIndex = 4;
+            this.labelNombreUsuario.Text = "label4";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::beat_on_jeans_escritorio.Properties.Resources.RectangleAzul;
+            this.pictureBox1.Location = new System.Drawing.Point(699, 225);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(149, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonCerrarTicket
+            // 
+            this.buttonCerrarTicket.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(196)))), ((int)(((byte)(182)))));
+            this.buttonCerrarTicket.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(196)))), ((int)(((byte)(182)))));
+            this.buttonCerrarTicket.FlatAppearance.BorderSize = 0;
+            this.buttonCerrarTicket.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCerrarTicket.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCerrarTicket.Location = new System.Drawing.Point(708, 231);
+            this.buttonCerrarTicket.Name = "buttonCerrarTicket";
+            this.buttonCerrarTicket.Size = new System.Drawing.Size(131, 38);
+            this.buttonCerrarTicket.TabIndex = 6;
+            this.buttonCerrarTicket.Text = "Cerrar Ticket";
+            this.buttonCerrarTicket.UseVisualStyleBackColor = false;
+            // 
+            // FormSoporte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(935, 618);
+            this.Controls.Add(this.buttonCerrarTicket);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelNombreUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelNombreTecnico);
+            this.Controls.Add(this.dataGridViewTickets);
+            this.Name = "FormSoporte";
             this.Text = "FormSoporte";
+            this.Load += new System.EventHandler(this.FormSoporte_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTickets)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewTickets;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Usuario_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tecnico_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TipoIncidencia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fecha_Creacion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fecha_Cierre;
+        private System.Windows.Forms.Label labelNombreTecnico;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelNombreUsuario;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonCerrarTicket;
     }
 }
--- beat_on_jeans_escritorio/FormSoporte.cs
+++ beat_on_jeans_escritorio/FormSoporte.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using beat_on_jeans_escritorio.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,106 @@ namespace beat_on_jeans_escritorio
         public FormSoporte()
         {
             InitializeComponent();
+        }
+
+        private void FormSoporte_Load(object sender, EventArgs e)
+        {
+            gridBorderRedondos();
+            esconderLabels();
+            cargarDatosGrid();
+            clickGrid();
+        }
+
+        // En el FormSoporte, modifica el método clickGrid y añade el manejador de eventos
+        private void clickGrid()
+        {
+            dataGridViewTickets.CellClick += DataGridViewTickets_CellClick;
+        }
+
+        private void DataGridViewTickets_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                // Ignorar clicks en los encabezados de columna
+                if (e.RowIndex < 0) return;
+
+                // Obtener la fila seleccionada
+                var row = dataGridViewTickets.Rows[e.RowIndex];
+
+                // Obtener los IDs del usuario y técnico
+                int usuarioId = Convert.ToInt32(row.Cells["Usuario_ID"].Value);
+                int tecnicoId = Convert.ToInt32(row.Cells["Tecnico_ID"].Value);
+
+                // Obtener los nombres
+                var (nombreUsuario, nombreTecnico) = TicketsOrm.GetNombresUsuarioYTecnico(usuarioId, tecnicoId);
+
+                // Mostrar los nombres en los labels
+                labelNombreUsuario.Text = nombreUsuario;
+                labelNombreTecnico.Text = nombreTecnico;
+
+                // Hacer visibles los labels
+                labelNombreTecnico.Visible = true;
+                label1.Visible = true;
+                labelNombreUsuario.Visible = true;
+                label2.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener los nombres: " + ex.Message,
+                              "Error",
+                              MessageBoxButtons.OK,
+                              MessageBoxIcon.Error);
+            }
+        }
+
+        private void cargarDatosGrid()
+        {
+            try
+            {
+                // Obtener los datos de los tickets con el tipo de incidencia
+                var tickets = TicketsOrm.SelectTicketsWithIncidentType();
+
+                // Configurar el DataGridView
+                dataGridViewTickets.AutoGenerateColumns = false;
+                dataGridViewTickets.DataSource = tickets;
+
+                // Configurar las propiedades de las columnas
+                dataGridViewTickets.Columns["ID"].DataPropertyName = "ID";
+                dataGridViewTickets.Columns["Usuario_ID"].DataPropertyName = "Usuario_ID";
+                dataGridViewTickets.Columns["Tecnico_ID"].DataPropertyName = "Tecnico_ID";
+                dataGridViewTickets.Columns["TipoIncidencia"].DataPropertyName = "TipoIncidencia";
+                dataGridViewTickets.Columns["Fecha_Creacion"].DataPropertyName = "Fecha_Creacion";
+                dataGridViewTickets.Columns["Fecha_Cierre"].DataPropertyName = "Fecha_Cierre";
+
+                // Formato de fechas
+                dataGridViewTickets.Columns["Fecha_Creacion"].DefaultCellStyle.Format = "dd/MM/yyyy HH:mm";
+                dataGridViewTickets.Columns["Fecha_Cierre"].DefaultCellStyle.Format = "dd/MM/yyyy HH:mm";
+
+
+                // Configuración de visualización
+                dataGridViewTickets.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+                dataGridViewTickets.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar los tickets: " + ex.Message,
+                              "Error",
+                              MessageBoxButtons.OK,
+                              MessageBoxIcon.Error);
+            }
+        }
+
+        private void esconderLabels()
+        {
+            labelNombreTecnico.Visible = false;
+            label1.Visible = false;
+            labelNombreUsuario.Visible = false;
+            label2.Visible = false;
+        }
+
+        private void gridBorderRedondos()
+        {
+            DataGridViewBordeRedondo.RedondearBordes(dataGridViewTickets, 20);
         }
     }
 }
